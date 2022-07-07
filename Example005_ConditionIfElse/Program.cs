138d0f24cb139a184cd89c8f4e58e5ed507366e2
alexey <alexey_borisoff@mail.ru>
2022-07-07 22:43:19 +0300

    example 006 base

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username == "Алексей");
+{
+    Console.WriteLine("Ура, это же Алексей!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
